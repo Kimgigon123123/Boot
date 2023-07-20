0ee4da82774d1f791fd41a65ba5cde00b23b9952
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-07-20 17:53:45 +0900

    720
    720

---
+++ smart/src/main/java/smart/controller/HomeController.java
@@ -0,0 +1,23 @@
+package smart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/myinfo")
+	public String myinfo(Model model) {
+		model.addAttribute("name","홍길동");
+		model.addAttribute("gender","<b>남성</b>");
+		return "member/myinfo";
+	}
+	
+}
